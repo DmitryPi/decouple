4db7ec162148071bb8171df55122998e4a50c0f2
Dmitry G <sdprecovery@gmail.com>
2023-01-23 10:37:13 +0300

    Create billing model Invoice; Create billing model ItemLine;

--- decoup/billing/models.py
+++ decoup/billing/models.py
@@ -1,3 +1,22 @@
+from django.conf import settings
 from django.db import models
 
-# Create your models here.
+
+class Invoice(models.Model):
+    class State(models.TextChoices):
+        PAID = "PAID"
+        UNPAID = "UNPAID"
+        CANCELLED = "CANCELLED"
+
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.PROTECT)
+    date = models.DateTimeField()
+    due_date = models.DateTimeField()
+    state = models.CharField(max_length=15, choices=State.choices, default=State.UNPAID)
+
+
+class ItemLine(models.Model):
+    invoice = models.ForeignKey(Invoice, on_delete=models.CASCADE)
+    quantity = models.IntegerField()
+    description = models.TextField(max_length=500)
+    price = models.DecimalField(max_digits=8, decimal_places=2)
+    taxed = models.BooleanField()
--- decoup/settings/base.py
+++ decoup/settings/base.py
@@ -47,6 +47,7 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
     # local apps
     "decoup.users",
+    "decoup.billing",
 ]
 
 MIDDLEWARE = [
