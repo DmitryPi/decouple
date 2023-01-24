7cc84ca5ccad45c8dae42598a0f06b2891ec3813
Dmitry G <sdprecovery@gmail.com>
2023-01-24 11:03:35 +0300

    Security update

---
+++ decoup/settings/production.py
@@ -0,0 +1,14 @@
+from .base import *  # noqa
+from .base import env
+
+# GENERAL
+# ------------------------------------------------------------------------------
+# https://docs.djangoproject.com/en/dev/ref/settings/#secret-key
+SECRET_KEY = env("DJANGO_SECRET_KEY")
+# https://docs.djangoproject.com/en/dev/ref/settings/#allowed-hosts
+ALLOWED_HOSTS = env.list("DJANGO_ALLOWED_HOSTS", default=["decoup.ru"])
+
+
+# SECURITY
+# ------------------------------------------------------------------------------
+STATIC_ROOT = env("STATIC_ROOT")
--- decoup/vue_spa/src/main.js
+++ decoup/vue_spa/src/main.js
@@ -1,6 +1,8 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from 'vue';
+import VueCookies from 'vue-cookies';
+import App from './App.vue';
 
+Vue.use(VueCookies);
 Vue.config.productionTip = false
 
 new Vue({
